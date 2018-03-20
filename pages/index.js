3a1269033073d93931f2011fc9cf64bbe37304b2
Marcin Sirocki <m.sirocki@madkom.pl>
2018-03-20 17:37:06 +0100

    send preloaded state to client

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import withRedux from "next-redux-wrapper";
 import {Col} from "reactstrap";
+import fetch from 'node-fetch';
 
 import Menu from "../components/Menu";
 import Article from "../components/Article";
@@ -8,11 +9,15 @@ import Layout from "../components/Layout";
 
 import makeStore from '../redux/store';
 import {selectMenuItem} from "../redux/modules/menuSelected";
-import {fetchArticle} from "../redux/modules/article";
+import {loadArticle} from "../redux/modules/article";
 
 class Index extends React.Component {
     static getInitialProps({store, query}) {
-        store.dispatch(fetchArticle(query.id));
+        store.dispatch(selectMenuItem('37a48fec-bc98-4848-b4e2-338d2e39fd23'));
+
+        return fetch('http://localhost:3000/api/article/37a48fec-bc98-4848-b4e2-338d2e39fd23')
+            .then(data => data.json())
+            .then(article => store.dispatch(loadArticle(article)));
     }
 
     componentWillReceiveProps(nextProps) {
--- redux/store.js
+++ redux/store.js
@@ -4,6 +4,12 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import {initialState, rootEpic, rootReducer} from "./modules";
 import {createEpicMiddleware} from 'redux-observable';
 
+let init = initialState;
+
+if (typeof window !== 'undefined') {
+    init = __NEXT_DATA__.props.initialState;
+}
+
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
-export default () => createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(epicMiddleware)));
+export default () => createStore(rootReducer, init, composeWithDevTools(applyMiddleware(epicMiddleware)));
