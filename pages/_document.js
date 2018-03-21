e657180d336937a2ad812d6ad9fe6eae8f6a6af8
Marcin Sirocki <m.sirocki@madkom.pl>
2018-03-21 17:05:11 +0100

    add custom global styles

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@ import Document, {Head, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/styles.scss';
 
 export default class ArticlesDocument extends Document {
     static getInitialProps({renderPage}) {
@@ -17,6 +18,7 @@ export default class ArticlesDocument extends Document {
             <Head>
                 <title>Articles</title>
                 <style dangerouslySetInnerHTML={{__html: bootstrap}}/>
+                <style dangerouslySetInnerHTML={{__html: styles}}/>
                 {this.props.styleTags}
             </Head>
             <body>
